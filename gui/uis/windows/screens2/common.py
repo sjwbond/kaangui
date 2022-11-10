142b5e909d592486b791c48c306d43579689e5a9
Jean Qussa <someone38472@gmail.com>
2022-11-10 07:57:19 +0100

    Remove self parameter from functions

--- gui/uis/windows/screens2/common.py
+++ gui/uis/windows/screens2/common.py
@@ -1,9 +1,9 @@
 from qt_core import *
 
 
-def parseDate(self, date: str) -> QDate:
+def parseDate(date: str) -> QDate:
     y, m, d = date.split("-")
     return QDate(int(y), int(m), int(d))
 
-def formatDate(self, date: QDate) -> str:
+def formatDate(date: QDate) -> str:
     return f"{date.year()}-{date.month():02}-{date.day():02}"
