5286f4d4f37afe56a51e344eb6fe9e26fb423419
Jean Qussa <someone38472@gmail.com>
2022-11-21 10:16:42 +0100

    Add remaining execution page tests

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -74,6 +74,102 @@ test_model_1 = {
     }
 }
 
+test_simulation_1 = {
+    "Competition": {},
+    "Diagnostics": {
+        "New Diagnostic": {
+            "bertrand_pricing": False,
+            "binary_files": False,
+            "binding_contingencies": False,
+            "clear_existing_diagnostics": True,
+            "congestion_charges": False,
+            "constraint_decomposition": False,
+            "constraint_rollover": False,
+            "execution_times": True,
+            "future_cost_function": False,
+            "heat_rate": False,
+            "infeasibility_diagnostic_files": False,
+            "interleaved_run_mode": False,
+            "interrupsion_sharing": False,
+            "load_includes_losses_iterations": False,
+            "load_increment": "-1",
+            "load_increment_2": "100",
+            "lp_files": False,
+            "lp_solver": False,
+            "lt_plan_annuities": False,
+            "lt_plan_npv": False,
+            "marginal_expansion_unit": False,
+            "marginal_loss_charges": False,
+            "marginal_unit": False,
+            "marginal_unit_transmission_detail": "Regional",
+            "max_infeasibility_log_lines": "-1",
+            "mip_solver": True,
+            "mps_files": False,
+            "mt_schedule_new_entry": False,
+            "nashcournot": False,
+            "objective_function": False,
+            "pasa_outage_patterns": False,
+            "ptdf_shift_factors": False,
+            "random_number_seed": False,
+            "region_supply": False,
+            "revenue_recovery": False,
+            "rsi_bidcost_markup": False,
+            "solution_files": False,
+            "solver_summary": False,
+            "step_from": "1",
+            "step_to": "-1",
+            "summary_each_sample": False,
+            "summary_each_step": True,
+            "task_components": False,
+            "task_size": True,
+            "transmission_loss_matrices_and_convergence": False,
+            "uniform_pricing": False,
+            "unit_commitment": False,
+            "unserved_energy": False,
+            "write_sample_weights": False,
+            "write_scenario_tree": False,
+            "write_with_generic_names": False
+        }
+    },
+    "Execution Plans": {
+        "New Execution Plans": {
+            "Settings": "",
+            "Simulation": {
+                "Horizons": "",
+                "LRMC Model": "",
+                "MT Schedule": "",
+                "PASA": "",
+                "Reports": "",
+                "ST Schedule": "",
+                "Scenarios": {}
+            }
+        }
+    },
+    "Horizons": {
+        "New Horizon": {
+            "End Date": "2022-01-01",
+            "Granularity": 555,
+            "Historic Sample Years": [],
+            "Periodicity": {
+                "Off": 2,
+                "On": 1,
+                "Type": "Year"
+            },
+            "Start Date": "2022-01-01"
+        }
+    },
+    "LRMC Models": {},
+    "MT Schedule": {},
+    "PASA": {},
+    "Performance": {},
+    "Production": {},
+    "Reports": {},
+    "ST Schedule": {},
+    "Settings": {},
+    "Stochastics": {},
+    "Transmission": {}
+}
+
 def create_test_widget(qtbot):
     widget = MainWindow()
     qtbot.addWidget(widget)
--- tests/test_execution_page.py
+++ tests/test_execution_page.py
@@ -1,6 +1,6 @@
 from main import MainWindow
 from qt_core import *
-from . fixtures import create_test_widget
+from . fixtures import create_test_widget, test_simulation_1
 
 def create_object(widget: MainWindow, row1: int, row2: int, name: str, type: str):
     item = get_node_item(widget, row1, row2)
@@ -97,6 +97,131 @@ def test_save_prompt_if_changed(qtbot, monkeypatch):
     new_data = idx.data(Qt.UserRole + 1)
     assert old_data != new_data
 
-# test_undo_redo
-# test_set_simulation
-# test_get_simulation
+def test_rename(qtbot, monkeypatch):
+    widget = create_test_widget(qtbot)
+    
+    # Switch to execution page
+    qtbot.mouseClick(widget.ui.left_menu.top_layout.itemAt(3).widget(), Qt.MouseButton.LeftButton)
+
+    # Create new Horizon object
+    item = get_node_item(widget, 2, 3)
+    widget.execution_controller.create_new(item, "Horizon", "horizons")
+
+    # Rename object
+    item = get_object_item(widget, 2, 3, 0)
+    monkeypatch.setattr(QInputDialog, "getText", lambda *args, **args2: ("My Horizon", True))
+    widget.execution_controller.rename(item)
+
+    # Make sure object name has changed
+    assert item.data(Qt.DisplayRole) == "My Horizon"
+
+def test_delete(qtbot, monkeypatch):
+    widget = create_test_widget(qtbot)
+    
+    # Switch to execution page
+    qtbot.mouseClick(widget.ui.left_menu.top_layout.itemAt(3).widget(), Qt.MouseButton.LeftButton)
+
+    # Create new Horizon object
+    item = get_node_item(widget, 2, 3)
+    widget.execution_controller.create_new(item, "Horizon", "horizons")
+
+    # Delete object
+    idx = get_object_index(widget, 2, 3, 0)
+    monkeypatch.setattr(QMessageBox, "question", lambda *args, **args2: QMessageBox.Yes)
+    widget.execution_controller.delete(idx)
+
+    # Make sure object no longer exists
+    idx = get_object_index(widget, 2, 3, 0)
+    assert idx.data(Qt.DisplayRole) is None
+
+def test_save_prompt_if_changed(qtbot, monkeypatch):
+    widget = create_test_widget(qtbot)
+    
+    # Switch to execution page
+    qtbot.mouseClick(widget.ui.left_menu.top_layout.itemAt(3).widget(), Qt.MouseButton.LeftButton)
+
+    # 1. Create new Horizon object
+    item = get_node_item(widget, 2, 3)
+    widget.execution_controller.create_new(item, "Horizon", "horizons")
+
+    # Select newly created object
+    idx = get_object_index(widget, 2, 3, 0)
+    widget.execution_tree.selectionModel().clear()
+    widget.execution_tree.selectionModel().select(idx, QItemSelectionModel.Select | QItemSelectionModel.Rows)
+    widget.execution_controller.selection_changed()
+
+    # Store object state
+    old_data = idx.data(Qt.UserRole + 1)
+
+    # 2. Modify object
+    widget.execution_controller.right_side_screen.onSpinBox.setValue(654)
+
+    # Select different node and save
+    monkeypatch.setattr(QMessageBox, "question", lambda *args, **args2: QMessageBox.Yes)
+    idx = get_node_index(widget, 1, 3)
+    widget.execution_tree.selectionModel().clear()
+    widget.execution_tree.selectionModel().select(idx, QItemSelectionModel.Select | QItemSelectionModel.Rows)
+    widget.execution_controller.selection_changed()
+
+    # Make sure object has changed
+    new_data = idx.data(Qt.UserRole + 1)
+    assert old_data != new_data
+
+    # 3. Rename object
+    item = get_object_item(widget, 2, 3, 0)
+    monkeypatch.setattr(QInputDialog, "getText", lambda *args, **args2: ("My Horizon", True))
+    widget.execution_controller.rename(item)
+
+    # Make sure object name has changed
+    assert item.data(Qt.DisplayRole) == "My Horizon"
+
+    # Undo 3 (Rename object)
+    widget.execution_controller.undo()
+    assert item.data(Qt.DisplayRole) == "New Horizon"
+    
+    # Redo 3 (Rename object)
+    widget.execution_controller.redo()
+    assert item.data(Qt.DisplayRole) == "My Horizon"
+
+    # Undo 3 again (Rename object)
+    widget.execution_controller.undo()
+    assert item.data(Qt.DisplayRole) == "New Horizon"
+
+    # Undo 2 (Modify object)
+    widget.execution_controller.undo()
+    idx = get_object_index(widget, 2, 3, 0)
+    undone_state = idx.data(Qt.UserRole + 1)
+    assert undone_state == old_data
+
+    # Undo 1 (Create new Horizon object)
+    widget.execution_controller.undo()
+    idx = get_object_index(widget, 2, 3, 0)
+    assert idx.data(Qt.UserRole + 1) is None
+
+    # Redo 1 (Create new Horizon object)
+    widget.execution_controller.redo()
+    idx = get_object_index(widget, 2, 3, 0)
+    assert idx.data(Qt.UserRole + 1) == old_data
+
+def test_set_get_simulation(qtbot):
+    widget = create_test_widget(qtbot)
+    
+    # Switch to execution page
+    qtbot.mouseClick(widget.ui.left_menu.top_layout.itemAt(3).widget(), Qt.MouseButton.LeftButton)
+
+    # Load a simulation
+    widget.execution_controller.set_simulation(test_simulation_1)
+
+    # Make sure objects exist
+    idx = get_object_index(widget, 0, 0, 0)
+    assert idx.data(Qt.DisplayRole) == "New Execution Plans"
+    idx = get_object_index(widget, 1, 1, 0)
+    assert idx.data(Qt.DisplayRole) == "New Diagnostic"
+    idx = get_object_index(widget, 2, 3, 0)
+    assert idx.data(Qt.DisplayRole) == "New Horizon"
+
+    # Get simulation
+    simulation = widget.execution_controller.get_simulation()
+
+    # Make sure simulation is equal to loaded simulation
+    assert simulation == test_simulation_1
